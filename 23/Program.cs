ae77f333e3109a4033d36461ac95b1ccc4a0e34e
avkirilin <nerosashok@mail.ru>
2022-12-24 23:13:09 +0300

    решена третья задача домашнего задания

---
+++ 23/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число N");
+        int N = Convert.ToInt32(Console.ReadLine());
+        int i = 0;
+        Console.Write($"Таблица кубов чисел от 1 до {N}: ");
+        for (i = 1; i <= N; i++)
+        {
+            if (i < N) Console.Write($"{Math.Pow(i, 3)}, ");
+            if (i == N) Console.WriteLine(Math.Pow(i, 3));
+        }
